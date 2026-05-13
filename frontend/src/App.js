8e89482c25e3a8620a8a05fbe6fabae5380b01f5
LJS5741 <ljs1051101@naver.com>
2026-05-13 20:44:52 +0900

    2026.05.13 헤더 푸터 및 메인페이지 전부 수정완료.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Header from "./components/Header";
+import HeroSection from "./components/HeroSection";
+import PopularRegions from "./components/PopularRegions";
+import CommunitySection from "./components/CommunitySection";
+import Footer from "./components/Footer";
+
+import "./styles/index.css";
+import "./styles/Header.css";
+import "./styles/HeroSection.css";
+import "./styles/PopularRegions.css";
+import "./styles/CommunitySection.css";
+import "./styles/Footer.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div>
+        <Header />
+
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <HeroSection />
+                <PopularRegions />
+                <CommunitySection />
+              </>
+            }
+          />
+        </Routes>
+
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 }
 
